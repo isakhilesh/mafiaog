4ccb7394f843a7a44e34d08f0f7c05a1f1fac7b7
Akhilesh Moorthy <akhileshmoorthy2010@gmail.com>
2023-10-05 20:52:53 -0700

    Tunnel added
    When the square moves closer to the tunnel entrance, press H to resize it and then move it through the tunnel

--- mafia-alpha/Assets/Scripts/PrimaryObjectScript.cs
+++ mafia-alpha/Assets/Scripts/PrimaryObjectScript.cs
@@ -7,6 +7,7 @@ public class PrimaryObjectScript : MonoBehaviour
     public float primaryObjectSpeed;
     private Rigidbody2D rb;
     private Vector2 primaryObjectDirection;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,11 +17,14 @@ public class PrimaryObjectScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float directionX = Input.GetAxisRaw("Horizontal"); // Capture horizontal input
         float directionY = Input.GetAxisRaw("Vertical");
-        primaryObjectDirection = new Vector2(0, directionY).normalized;
+
+        primaryObjectDirection = new Vector2(directionX, directionY).normalized; // Combine horizontal and vertical input
     }
+
     void FixedUpdate()
     {
-        rb.velocity = new Vector2(0, primaryObjectDirection.y * primaryObjectSpeed);
+        rb.velocity = new Vector2(primaryObjectDirection.x * primaryObjectSpeed, primaryObjectDirection.y * primaryObjectSpeed);
     }
 }
