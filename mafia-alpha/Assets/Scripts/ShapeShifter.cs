ae4e299e633cf43f7cba49de0ddb737a2ab45d9d
Akhilesh Moorthy <akhileshmoorthy2010@gmail.com>
2023-10-06 10:55:10 -0700

    Press R to transform to Rectangle and C to turn into a Circle

--- mafia-alpha/Assets/Scripts/ShapeShifter.cs
+++ mafia-alpha/Assets/Scripts/ShapeShifter.cs
@@ -6,6 +6,7 @@ public class ShapeShifter : MonoBehaviour
 {
     private Vector3 originalScale;
     private bool isRectangle = false;
+    private bool isCircle = false;
 
     private void Start()
     {
@@ -14,13 +15,33 @@ public class ShapeShifter : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.H))
+        if (Input.GetKeyDown(KeyCode.R))
         {
             isRectangle = !isRectangle;
             if (isRectangle)
             {
                 // Change the square to a rectangle by modifying the scale
                 transform.localScale = new Vector3(originalScale.x * 0.5f, originalScale.y * 0.15f, originalScale.z);
+            }
+            else
+            {
+                // Change it back to a square
+                transform.localScale = originalScale;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            // Turn the square into a circle by setting both X and Y scales to the same value
+            isCircle = !isCircle;
+            float circleScale = 0.5f; // Adjust this value as needed
+
+           
+
+            if (isRectangle)
+            {
+                // Change the square to a circle by modifying the scale
+                transform.localScale = new Vector3(originalScale.x * circleScale, originalScale.x * circleScale, originalScale.z);
             }
             else
             {
