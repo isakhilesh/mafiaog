8c2b99ea52a3e578609af1e1a12cec749ef727c1
RishiRewadkar <rewadkar@usc.edu>
2023-10-11 13:47:36 -0700

    EnemyPush+CCTVCamera+CameraFollow

---
+++ mafia-alpha/Assets/FollowCamera.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public Transform player; // Reference to the player's transform
+    public Vector3 offset = new Vector3(0f, 5f, -10f); // Offset to control the camera's position
+
+    void Update()
+    {
+        if (player != null) // Ensure the player reference is not null
+        {
+            // Set the camera's position to match the player's position with an offset
+            transform.position = player.position + offset;
+        }
+    }
+}
