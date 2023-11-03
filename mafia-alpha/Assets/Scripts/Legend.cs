12ad1b636d166bb9680172420498f0978a2f9fa0
Nachiket Erlekar <nachiketerlekar@Nachikets-MacBook-Air.local>
2023-11-02 23:22:10 -0700

    Integration of levels

---
+++ mafia-alpha/Assets/Scripts/Legend.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Legend : MonoBehaviour
+{
+    public GameObject panel; // Reference to the UI Panel (should include the text as a child)
+
+    private bool isInstructionsVisible = false;
+
+    void Start()
+    {
+        panel.SetActive(false); // Start with the panel and text hidden
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            isInstructionsVisible = !isInstructionsVisible;
+
+            if (isInstructionsVisible)
+            {
+                panel.SetActive(true); // Show the UI panel and text
+                Time.timeScale = 0.0f; // Pause the game
+            }
+            else
+            {
+                panel.SetActive(false); // Hide the UI panel and text
+                Time.timeScale = 1.0f; // Unpause the game
+            }
+        }
+    }
+
+    private void OnGUI(){
+        if(isInstructionsVisible){
+
+        Rect gameOverRect = new Rect(Screen.width / 2 - 100, Screen.height / 2 - 25, 400, 100);
+
+        GUIStyle style = new GUIStyle(GUI.skin.label);
+        style.fontSize = 16;
+        style.alignment = TextAnchor.MiddleCenter;
+        style.normal.textColor = Color.black;
+        GUI.backgroundColor = Color.yellow;
+        GUI.Label(gameOverRect,"Space Bar : Anti-Gravity \nDown Arrow : Shrink \nUp Arrow : Grow and Destroy \nM Key : Activate Magnet ", style);
+
+        }
+    }
+}
